6878e97d4f0428ae607d0e49095ab05d0ec3279f
Christina <christina-jung@hotmail.com>
2020-09-09 22:00:27 +0200

    supply list more structured

--- src/components/Views/PurchasingPage.vue
+++ src/components/Views/PurchasingPage.vue
@@ -7,30 +7,46 @@
         kannst Du Artikel, die gekauft werden müssen, in die Einkaufsliste eintragen.
         Deine Mitbewohner werden per Push-Benachrichtigung informiert!
       </span>
-      <v-card flat class="pa-3">
-        <v-row wrap justify-space-around>
-          <v-col xs="12" sm="12" md="6">
-            <div class="caption grey--text">Einkaufsliste:</div>
-            <div>
-              <v-checkbox 
-              v-model="checkbox" 
-              v-for="n in 3" 
-              :key="n" 
-              :label="`Checkbox 1: ${checkbox.toString()}`"
-              :value="n">
-          
-              </v-checkbox>
-             
-            </div>
-          </v-col>
-          <v-col xs="12" sm="6" md="3">
-            <div>Hier kannst Du Artikel, dichtigung informiert!</div>
-          </v-col>
-          <v-col xs="12" sm="6" md="3">
-            <div>Hböabaöbfö</div>
-          </v-col>
-        </v-row>
-      </v-card>
+
+      <v-row wrap justify-space-around>
+        <v-col xs="12" sm="12" md="6">
+          <v-card flat class="pa-3">
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left"></th>
+                    <th class="text-left">Bezeichnung</th>
+                    <th class="text-left">Status</th>
+                    <th class="text-left"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="item in desserts" :key="item.name">
+                    <v-checkbox v-model="checkbox"></v-checkbox>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.calories }}</td>
+                    <v-btn text>
+                      <v-icon>mdi-check</v-icon>
+                    </v-btn>
+                    <v-btn text>
+                      <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+
+           
+          </v-card>
+        </v-col>
+        <v-col xs="12" sm="6" md="3">
+          <div>Hier kannst Du Artikel, dichtigung informiert!</div>
+        </v-col>
+        <v-col xs="12" sm="6" md="3">
+          <div>Hböabaöbfö</div>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -42,6 +58,20 @@ export default {
       checkbox: true,
       radioGroup: 1,
       switch1: true,
+      desserts: [
+        {
+          name: "Spüli",
+          calories: false,
+        },
+        {
+          name: "Müllsäcke",
+          calories: false,
+        },
+        {
+          name: "Ingwerbröd",
+          calories: false,
+        },
+      ],
     };
   },
 };
