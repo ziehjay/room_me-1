25f8dd0332c8ac1b1de65891da53f7e2a9c06c43
Christina <christina-jung@hotmail.com>
2020-09-09 19:24:17 +0200

    color theme added

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,5 +5,10 @@ import '@mdi/font/css/materialdesignicons.css';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    
+    iconfont: 'md',
+    theme: {
+        darkblue: '#241c24',
+        green: "#315458",
+        orange: '#B1493C',
+    }
 });
