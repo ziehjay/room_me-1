7f68cf14d88af847c56035e191404d498a331c3e
Christina <christina-jung@hotmail.com>
2020-09-09 11:48:35 +0200

    added routing to settings

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@
                     </v-list-item-content>
                 </v-list-item>
 
-                <v-list-item>
+                <v-list-item to="/Settings">
                     <v-list-item-action>
                         <v-icon>mdi-account</v-icon>
                     </v-list-item-action>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Home from "./components/Views/Home";
 
 import Login from "./components/Views/Login";
 import ToDoList from "./components/Views/ToDoList";
+import Settings from "./components/Views/Settings";
 
 Vue.use(VueRouter);
 
@@ -11,7 +12,8 @@ Vue.use(VueRouter);
 const routes = [
     {path: '/home', component: Home},
     {path: '/login', component: Login},
-    {path: '/todo', component: ToDoList}
+    {path: '/todo', component: ToDoList},
+    {path: '/settings', component: Settings}
 ];
 
 
