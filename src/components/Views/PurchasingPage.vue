c5608add9e05b5b7838225f9bf3493f1338e8b02
Christina <christina-jung@hotmail.com>
2020-09-10 13:09:05 +0200

    some design changes

--- src/components/Views/PurchasingPage.vue
+++ src/components/Views/PurchasingPage.vue
@@ -67,35 +67,23 @@
         </v-col>
         <v-col xs="12" sm="6" md="3">
           <v-card elevation="24" max-width="444" class="mx-auto">
-            <v-system-bar lights-out></v-system-bar>
+            <v-system-bar lights-on>Erledigt</v-system-bar>
             <v-carousel
               :continuous="false"
               :cycle="cycle"
-              :show-arrows="false"
+              :show-arrows="true"
               hide-delimiter-background
-              delimiter-icon="mdi-minus"
-              height="300"
+              height="250"
             >
               <v-carousel-item v-for="(slide, i) in slides" :key="i">
+              
                 <v-sheet :color="colors[i]" height="100%" tile>
-                  <v-row class="fill-height" align="center" justify="center">
-                    <div class="display-3">{{ slide }} Slide</div>
+                  <v-row class="fill-height" align="start" justify="start">
+                    <div class="display-3">{{ slide }} +</div>
                   </v-row>
                 </v-sheet>
               </v-carousel-item>
             </v-carousel>
-            <v-list two-line>
-              <v-list-item>
-                <v-list-item-avatar>
-                  <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
-                </v-list-item-avatar>
-                <v-list-item-content>
-                  <v-list-item-title>Spüli, Klopapier</v-list-item-title>
-                  <v-list-item-subtitle>Gekauft am 03.05.20</v-list-item-subtitle>
-                </v-list-item-content>
-                <v-list-item-action></v-list-item-action>
-              </v-list-item>
-            </v-list>
           </v-card>
         </v-col>
       </v-row>
@@ -149,14 +137,11 @@ export default {
         },
       ],
       colors: [
-        "green",
-        "secondary",
-        "yellow darken-4",
-        "red lighten-2",
-        "orange darken-1",
+        "darkblue",
+   
       ],
       cycle: false,
-      slides: ["First", "Second", "Third", "Fourth", "Fifth"],
+      slides: ["Uno", "Second", "Third", "Fourth", "Fifth"],
     };
   },
 };
